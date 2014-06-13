b86ea2b71943e57698f4cd17be312d4082504578
Daniel Malon <daniel.malon@me.com>
2014-06-13 11:23:18 +0100

    add basic tiers management

---
+++ models/tier.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var mongoose_uuid = require('mongoose-uuid');
+
+
+function tierModel () {
+  var tierSchema = mongoose.Schema({
+    environment: { type: String, ref: 'Environment', index: true },
+    platform: { type: String, ref: 'Platform', index: true },
+    name: { type: String },
+    system_name: { type: String },
+    base_image: { type: String },
+    home_network: { type: String }
+  }, { _id: false, versionKey: false });
+
+  tierSchema.plugin(mongoose_uuid.plugin, 'Tier');
+
+  tierSchema.pre('save', function (next) {
+    var tier = this;
+
+    if (tier.system_name && tier.system_name.length > 0) {
+      next();
+      return;
+    }
+
+    tier.system_name = tier.name.toLowerCase().replace(/\s+/g, '_');
+
+    next();
+  });
+
+  return mongoose.model('Tier', tierSchema);
+}
+
+module.exports = new tierModel();
---
+++ routes/inventory/tier.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+
+
+module.exports = function (router) {
+  var Tier = mongoose.models.Tier;
+
+  router.get('/', function (req, res) {
+    Promise.props({
+      tiers: Tier.find().sort({ name: 1 }).exec()
+    }).then(function (models) {
+      res.render('tier/list', models);
+    });
+  });
+
+  router.get('/new', function (req, res) {
+    res.render('tier/new');
+  });
+
+  router.get('/:tier_id', function (req, res) {
+    Promise.props({
+      tier: Tier.findOne({ _id: req.param('tier_id') }).populate('environment platform').exec()
+    }).then(function (models) {
+      res.render('tier/show', models);
+    });
+  });
+
+  router.post('/', function (req, res) {
+    var tier = new Tier({
+      environment: req.param('environment'),
+      platform: req.param('platform'),
+      name: req.param('name'),
+      base_image: req.param('base_image'),
+      home_network: req.param('home_network')
+    });
+
+    tier.save(function (err) {
+      if (err) {
+        res.render('error', {
+          message: 'save failed',
+          error: err,
+          url: req.url
+        });
+
+        return;
+      }
+
+      req.flash('success', 'Tier "%s" has been created', tier.name);
+
+      res.redirect('/tier/' + tier.id);
+    });
+  });
+
+  router.delete('/:tier_id', function (req, res) {
+    Promise.props({
+      tier: Tier.findOne({ _id: req.param('tier_id') }).exec()
+    }).then(function (models) {
+      models.tier.remove();
+
+      req.flash('success', 'Tier "%s" has been deleted', models.tier.name);
+
+      res.redirect('/tier');
+    });
+  });
+};
