7ad129b6f8e040f13e6eb7b47f2ea0255ab2b6d1
Daniel Malon <daniel.malon@me.com>
2014-06-13 10:41:59 +0100

    add basic environments management

---
+++ models/environment.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var mongoose_uuid = require('mongoose-uuid');
+var smartdc = require('smartdc');
+
+
+function environmentModel () {
+  var environmentSchema = mongoose.Schema({
+    name: { type: String, unique: true },
+    system_name: { type: String, unique: true },
+    pkgcloud_options: {}
+  }, { _id: false, versionKey: false });
+
+  environmentSchema.plugin(mongoose_uuid.plugin, 'Environment');
+
+  environmentSchema.pre('save', function (next) {
+    var environment = this;
+
+    if (environment.system_name && environment.system_name.length > 0) {
+      next();
+      return;
+    }
+
+    environment.system_name = environment.name.toLowerCase().replace(/\s+/g, '_');
+
+    next();
+  });
+
+  var _cacheCloudClient = {};
+
+  environmentSchema.methods.getCompute = function () {
+    var environment = this;
+
+    if (_cacheCloudClient[environment.id]) {
+      return _cacheCloudClient[environment.id];
+    }
+
+    var options = {
+      user: environment.pkgcloud_options.account,
+      url: environment.pkgcloud_options.url,
+      cacheExpiry: environment.pkgcloud_options.cacheExpiry || 60
+    };
+
+    if (!options.url.match(/^https?:\/\//)) {
+      options.url = 'https://' + options.url;
+    }
+
+    if (environment.pkgcloud_options.key) {
+      options.sign = smartdc.privateKeySigner({
+        key: environment.pkgcloud_options.key,
+        keyId: environment.pkgcloud_options.keyId,
+        user: environment.pkgcloud_options.account
+      });
+    } else {
+      options.sign = smartdc.sshAgentSigner({
+        keyId: environment.pkgcloud_options.keyId,
+        user: environment.pkgcloud_options.account
+      });
+    }
+
+    _cacheCloudClient[environment.id] = smartdc.createClient(options);
+
+    return _cacheCloudClient[environment.id];
+  };
+
+  return mongoose.model('Environment', environmentSchema);
+}
+
+module.exports = new environmentModel();
---
+++ routes/inventory/environment.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+
+
+module.exports = function (router) {
+  var Environment = mongoose.models.Environment;
+
+  router.get('/', function (req, res) {
+    Promise.props({
+      environments: Environment.find().sort({ name: 1 }).exec()
+    }).then(function (models) {
+      res.render('environment/list', models);
+    });
+  });
+
+  router.get('/new', function (req, res) {
+    res.render('environment/new');
+  });
+
+  router.get('/:environment_id', function (req, res) {
+    Promise.props({
+      environment: Environment.findOne({ _id: req.param('environment_id') }).exec()
+    }).then(function (models) {
+      res.render('environment/show', models);
+    });
+  });
+
+  router.post('/', function (req, res) {
+    var environment = new Environment({
+      name: req.param('name'),
+      pkgcloud_options: req.param('pkgcloud_options')
+    });
+
+    environment.save(function (err) {
+      if (err) {
+        res.render('error', {
+          message: 'save failed',
+          error: err,
+          url: req.url
+        });
+
+        return;
+      }
+
+      res.redirect('/environment/' + environment.id);
+    });
+  });
+
+  router.delete('/:environment_id', function (req, res) {
+    Promise.props({
+      environment: Environment.findOne({ _id: req.param('environment_id') }).exec()
+    }).then(function (models) {
+      models.environment.remove();
+
+      req.flash('success', 'Environment "%s" has been delted', models.environment.name);
+
+      res.redirect('/environment');
+    });
+  });
+};
