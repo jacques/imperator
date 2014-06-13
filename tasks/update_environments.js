b2553f39b278c36f924a7c608e9d59878f968667
Daniel Malon <daniel.malon@me.com>
2014-06-13 13:54:15 +0100

    add task for updating the machines for configured environments

---
+++ tasks/update_environments.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var _ = require('underscore');
+var async = require('async');
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var debug = require('debuglog')('imperator/tasks');
+
+
+module.exports = function updateEnvironments () {
+  var Environment = mongoose.models.Environment;
+  var Platform = mongoose.models.Platform;
+  var Tier = mongoose.models.Tier;
+  var Machine = mongoose.models.Machine;
+
+  var _lock = false;
+
+  function _updateEnvironmentsTask() {
+    debug('starting update');
+
+    if (_lock) {
+      debug('skipping because lock is set');
+
+      return false;
+    }
+
+    try {
+      debug('acquiring lock');
+
+      _lock = true;
+
+      Promise.props({
+        environments: Environment.find().exec(),
+        platforms: Platform.find().exec(),
+        tiers: Tier.find().exec(),
+        machines: Machine.find().exec()
+      }).then(function (models) {
+        async.each(models.environments, function iterEnvironment(environment, done) {
+          debug('updating %s', environment.name);
+
+          var cloud = environment.getCompute();
+
+          cloud.listMachines(function (err, machines) {
+            async.each(machines, function (item, item_done) {
+              debug('matching machine %s', item.id);
+
+              Machine.findOne({ machine_uuid: item.id }).exec()
+                .then(function (m) {
+                  if (!m) {
+                    debug('creating new machine for %s', item.id);
+
+                    m = new Machine({
+                      environment: environment.id,
+                      last_seen: Date.now(),
+                      name: item.name,
+                      machine_uuid: item.id,
+                      created: item.created,
+                      updated: item.updated,
+                      state: item.state || 'unknown'
+                    });
+                  } else {
+                    debug('updating machine %s for %s', m.id, item.id);
+
+                    if (item.state) {
+                      m.state = item.state;
+                    }
+                    m.created = item.created;
+                    m.updated = item.updated;
+                    m.last_seen = Date.now();
+                  }
+
+                  m.save(item_done);
+                });
+            }, function (err) {
+              done(err);
+            });
+          });
+        }, function callback(err) {
+          debug('releasing lock');
+
+          _lock = false;
+        });
+      });
+    } catch (err) {
+      debug('releasing lock');
+
+      _lock = false;
+    }
+  }
+
+  return {
+    run: function run (interval) {
+      setInterval(_updateEnvironmentsTask, 1000 * (interval || 60));
+
+      // run async on next tick
+      process.nextTick(_updateEnvironmentsTask);
+    },
+    update: function update (cb) {
+      debug('forcing an update');
+
+      _updateEnvironmentsTask();
+
+      if (cb) {
+        cb(null);
+      }
+    }
+  };
+};
