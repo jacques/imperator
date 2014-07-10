ab3bb9d500a37d8a23228b7f2ed6ec9520ad2ebb
Daniel Malon <daniel.malon@me.com>
2014-07-10 10:29:36 +0100

    editing of platforms

--- routes/inventory/platform.js
+++ routes/inventory/platform.js
@@ -29,6 +29,15 @@ module.exports = function (router) {
     });
   });
 
+  router.get('/:platform_id/edit', function (req, res) {
+    Promise.props({
+      platform: Platform.findOne({ _id: req.param('platform_id') }).exec(),
+      tiers: Tier.find({ platform: req.param('platform_id') }).exec()
+    }).then(function (models) {
+      res.render('platform/edit', models);
+    });
+  });
+
   router.post('/', function (req, res) {
     var platform = new Platform({
       environment: req.param('environment'),
@@ -49,6 +58,25 @@ module.exports = function (router) {
       req.flash('success', 'Platform "%s" has been created', platform.name);
 
       res.redirect('/platform/' + platform.id);
+    });
+  });
+
+  router.post('/:platform_id', function (req, res) {
+    Promise.props({
+      platform: Platform.findOne({ _id: req.param('platform_id') }).exec()
+    }).then(function (models) {
+      models.platform.environment = req.param('environment');
+      models.platform.name = req.param('name');
+
+      models.platform.save(function (err) {
+        if (err) {
+          req.flash('error', 'Platform "%s" could not be saved', models.platform.name);
+        } else {
+          req.flash('success', 'Platform "%s" has been saved', models.platform.name);
+        }
+
+        res.redirect('/platform/' + models.platform.id);
+      });
     });
   });
 
