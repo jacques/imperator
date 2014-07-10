c142cca241bb1f8e988f174acd600e9cf47b2f35
Daniel Malon <daniel.malon@me.com>
2014-07-10 10:55:17 +0100

    editing of tiers

--- routes/inventory/tier.js
+++ routes/inventory/tier.js
@@ -22,7 +22,7 @@ module.exports = function (router) {
   router.get('/new', function (req, res) {
     Promise.props({
       environment: Environment.findOne({ _id: req.param('environment') }).exec(),
-      networks: Network.find({ environment: req.param('environment') }).exec(),
+      networks: Network.find({ environment: req.param('environment') }).exec()
     }).then(function (models) {
       var cloud = models.environment.getCompute();
 
@@ -40,7 +40,24 @@ module.exports = function (router) {
       tier: Tier.findOne({ _id: req.param('tier_id') }).populate('environment platform').exec(),
       machines: Machine.find({ tier: req.param('tier_id') }).exec()
     }).then(function (models) {
+      console.dir(models);
       res.render('tier/show', models);
+    });
+  });
+
+  router.get('/:tier_id/edit', function (req, res) {
+    Promise.props({
+      tier: Tier.findOne({ _id: req.param('tier_id') }).populate('environment platform').exec()
+    }).then(function (models) {
+      var cloud = models.tier.environment.getCompute();
+
+      models.images = Promise.promisify(cloud.listImages, cloud)();
+      models.packages = Promise.promisify(cloud.listPackages, cloud)();
+      models.networks = Network.find({ environment: models.tier.environment.id }).exec();
+
+      Promise.props(models).then(function (models) {
+        res.render('tier/edit', models);
+      });
     });
   });
 
@@ -80,6 +97,41 @@ module.exports = function (router) {
       req.flash('success', 'Tier "%s" has been created', tier.name);
 
       res.redirect('/tier/' + tier.id);
+    });
+  });
+
+  router.post('/:tier_id', function (req, res) {
+    Promise.props({
+      tier: Tier.findOne({ _id: req.param('tier_id') }).exec()
+    }).then(function (models) {
+      var networks = req.param('networks');
+      var home_network = req.param('home_network');
+
+      if (!networks || networks.length === 0) {
+        networks = [home_network];
+      }
+
+      if (!_.contains(networks, home_network)) {
+        networks.push(home_network);
+      }
+
+      models.tier.environment = req.param('environment');
+      models.tier.platform = req.param('platform');
+      models.tier.name = req.param('name');
+      models.tier.base_image = req.param('base_image');
+      models.tier.base_package = req.param('base_package');
+      models.tier.home_network = home_network;
+      models.tier.networks = networks;
+
+      models.tier.save(function (err) {
+        if (err) {
+          req.flash('error', 'Tier "%s" could not be saved', models.tier.name);
+        } else {
+          req.flash('success', 'Tier "%s" has been saved', models.tier.name);
+        }
+
+        res.redirect('/tier/' + models.tier.id);
+      });
     });
   });
 
