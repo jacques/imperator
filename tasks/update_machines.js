5f270e8e4425bcf837850d113996f6ae04ab4ddd
Daniel Malon <daniel.malon@me.com>
2014-08-05 12:43:15 +0100

    only autoupdate machines managed by imperator

---
+++ tasks/update_machines.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var async = require('async');
+var mongoose = require('mongoose');
+var debug = require('debuglog')('imperator/tasks');
+
+
+module.exports = function updateMachines () {
+  var Machine = mongoose.models.Machine;
+
+  var _lock = false;
+
+  function _updateMachinesTask() {
+    debug('starting update');
+
+    if (_lock) {
+      debug('skipping because lock is set');
+
+      return false;
+    }
+
+    try {
+      debug('acquiring lock');
+
+      _lock = true;
+
+      Machine.find({ managed: true, state: { $ne: 'destroyed' } }, function (err, machines) {
+        async.each(machines, function iterMachines(machine, next) {
+          debug('updating machine %s', machine.name);
+
+          machine.updateFromEnvironment()
+            .catch(next)
+            .then(function () {
+              next();
+            });
+        });
+      });
+    } catch (err) {
+      debug('releasing lock');
+
+      _lock = false;
+    }
+  }
+
+  return {
+    run: function run (interval) {
+      setInterval(_updateMachinesTask, 1000 * (interval || 600));
+
+      // run async on next tick
+      process.nextTick(_updateMachinesTask);
+    },
+    update: function update (cb) {
+      debug('forcing an update');
+
+      _updateMachinesTask();
+
+      if (cb) {
+        cb(null);
+      }
+    }
+  };
+};
